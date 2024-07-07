c47c1d17ce4b38e80423d257566a3f0a829bd9d5
Harshit Mishra <harmishra@BLR.ALLEGISINDIA.COM>
2024-07-07 14:41:18 +0530

    for checking ruleset

--- ecom/src/component/Footer.js
+++ ecom/src/component/Footer.js
@@ -4,11 +4,12 @@ export default function Footer() {
   return (
     <div
       className="container-fluid p-3 fw-bold"
-      style={{ backgroundColor: "#e3f2fd" }}
+      data-bs-theme="dark"
+      style={{ backgroundColor: "#EEEEEE" }}
     >
       <div className="row">
         <div className="col-md-6 mb-2">
-          © 2021 TEKsystems Global Services. All Rights Reserved.
+          © 2024 Banglore Gang All Rights Reserved.
         </div>
         <div className="col-md-3 mb-2">Privacy Policy</div>
         <div className="col-md-3 v">Terms of Service</div>
